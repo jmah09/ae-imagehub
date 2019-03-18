66fe926584fa31c1d2b815d689e7cc9632d1772f
seraphinush <seraphinus.hong@gmail.com>
2019-03-17 17:18:48 -0700

    seraph' getInfo attempt

--- AEImageHub/ClientApp/src/componentsTemplate/Palette.js
+++ AEImageHub/ClientApp/src/componentsTemplate/Palette.js
@@ -23,7 +23,6 @@ export class Palette extends Component {
         this.GetUserImages = this.GetUserImages.bind(this);
         this.TrashSelectedImages = this.TrashSelectedImages.bind(this);
 
-
         this.GetUserImages();
     }
 
@@ -131,6 +130,100 @@ export class Palette extends Component {
                 redirect: true
             })
         }
+    }
+
+    // TODO
+    // GET INFO
+    onGetInfo() {
+        if (this.state.showInfo) {
+            this.setState({ showInfo: false });
+            return;
+        }
+
+        // TODO : fix to simply find a single instance of selected
+        const selected = this.state.photos.filter((value, index, array) => {
+            return value.selected;
+        });
+
+        if (selected.length > 0 && !this.state.showInfo) {
+            this.setState({ showInfo: true });
+        }
+    }
+
+    // TODO
+    handleGetInfoSubmit() {
+        //
+        
+        this.setState({ showInfo: false });
+    }
+
+    listItems(array) {
+        let str = array.shift() || '';
+    
+        array.forEach((item) => { str += ", " + item });
+    
+        return str;
+    }
+
+    // TODO
+    // 1. implement way to check whether there is anything in common
+    // 3. get list of projects in database
+    // 4. get list of classifications in database
+    renderGetInfo() {
+        if (this.state.showInfo) {
+            let values;
+
+            // TODO 1
+            const selected = this.state.photos.filter((value, index, array) => {
+                return value.selected;
+            });
+
+            return (
+                <div id="getInfo-container">
+                    <div id="getInfo-left">
+                    {selected.length > 1
+                        ? <img src="http://saveabandonedbabies.org/wp-content/uploads/2015/08/default.png" /> 
+                        : <img src={selected.photo.src} />}
+                    </div>
+
+                    <div id="getInfo-right">
+                        <p>TITLE : <br />
+                            <form type="text" id="name">{selected.length > 1 ? 'Various' : selected.imageName}</form>
+                        </p>
+                        <p>DATE : <br />
+                            {selected.length > 1 ? 'Various' : selected.uploadedDate}
+                        </p>
+                        <p>USER : <br />
+                            {selected.length > 1 ? 'Various' : selected.uId}
+                        </p>
+                        <p>PROJECT : <br />
+                            {selected.length > 1 ? 'Various' : this.listItems(selected.projectLink)}
+                            <select>
+                                <option name="projecta">Project A</option>
+                                <option name="projectb">Project B</option>
+                                <option name="projectc">Project C</option>
+                                <option name="projectd">Project D</option>
+                            </select>
+                        </p>
+                        <p>CLASSIFICATION : <br />
+                            {selected.length > 1 ? 'Various' : this.listItems(selected.tagLink)}
+                            <select>
+                                <option name="bridge">Bridge</option>
+                                <option name="dam">Dam</option>
+                                <option name="river">River</option>
+                                <option name="road">Road</option>
+                            </select>
+                        </p>
+                        <br /><br />
+                        <button onSubmit={this.handleGetInfoSubmit}>Submit</button>
+                        <br /><br />
+                        <button onClick={this.onGetInfo}>Cancel</button>
+                    </div>
+                </div>
+            );
+        }
+
+        return null;
     }
 
     // TODO
