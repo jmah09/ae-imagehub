671b6ba45a8bead112a22c48e50d7f9bc7a4312b
seraphinush <seraphinus.hong@gmail.com>
2019-03-26 14:46:30 -0700

    added content-control to major ui components

--- AEImageHub/ClientApp/src/componentsTemplate/Header.js
+++ AEImageHub/ClientApp/src/componentsTemplate/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { getCredentials, getToken } from '../adalConfig';
+
 import './Main.css';
 
 import logo from './img/logo.png';
@@ -9,6 +11,10 @@ export class Header extends Component {
 
     // TODO
     render() {
+
+        let token = getToken();
+        let userid = getCredentials(token).name;
+
         return (
             <div id="Header">
                 <div id="hdricon">
@@ -16,8 +22,10 @@ export class Header extends Component {
                         <Image src={logo} />
                     </Link>
                     <div id="hdrname">
-                        <span>NAME PLACEHOLDER</span>
-                        <button>SIGN OUT</button>
+                        <span>{userid}</span>
+                        <a href="about:blank" style={{textDecoration: 'none', color:'#FFF'}}>
+                            <button>SIGN OUT</button>
+                        </a>
                     </div>
                 </div>
             </div>
