6fcb0622b3ddd58cb4e0e0efb209cc5c399ba92c
seraphinush <seraphinus.hong@gmail.com>
2019-03-29 03:44:23 +0000

    Merge branch 'develop' into 'update/image-editor'
    # Conflicts:
    #   AEImageHub/ClientApp/src/componentsTemplate/Main.css

--- AEImageHub/ClientApp/src/componentsTemplate/ImageEditor/index.js
+++ AEImageHub/ClientApp/src/componentsTemplate/ImageEditor/index.js
@@ -1,137 +1,238 @@
 import React, { Component } from 'react';
-import 'cropperjs/dist/cropper.css';
+import { Title } from '../Title';
+import { TextInput } from '../sub-components/form-text-input';
 import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
 import axios from 'axios';
 import { getToken } from '../../adalConfig';
 import { Redirect } from 'react-router-dom';
 
+import './edit-image.css';
+
 export class ImageEditor extends Component {
-    constructor(props) {
-        super(props);
-        let params = new URLSearchParams(window.location.search);
-        let src = "/api/image/" + params.get('src');
-        this.state = {
-            src: src,
-            cropResult: null,
-            redirect: false,
-        };
-        this.cropImage = this.cropImage.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.rotateCW = this.rotateCW.bind(this);
-        this.rotateCounterCW = this.rotateCounterCW.bind(this);
-        this.submitImage = this.submitImage.bind(this);
-    }
 
-    onChange(e) {
-        e.preventDefault();
-        let files;
-        if (e.dataTransfer) {
-            files = e.dataTransfer.files;
-        } else if (e.target) {
-            files = e.target.files;
-        }
-        const reader = new FileReader();
-        reader.onload = () => {
-            this.setState({ src: reader.result });
-        };
-        reader.readAsDataURL(files[0]);
-    }
+  constructor(props)
+  {
+    super(props);
 
-    cropImage() {
-        if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
-            return;
-        }
-        this.setState({
-            cropResult: this.cropper.getCroppedCanvas().toDataURL(),
-        });
-    }
+    let params = new URLSearchParams(window.location.search);
+    let src = "/api/image/" + params.get('src');
 
-    submitImage() {    
-        var fd = new FormData();
-        fd.append('image', this.dataURItoBlob(this.cropper.getCroppedCanvas().toDataURL()), "edited image");
-        axios({
-            url: '/api/image/',
-            method: 'POST',
-            data: fd,
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'Authorization': "bearer " + getToken()
-            }
-        }).then(res => {
-            this.setState({
-                redirect: true
-            })
-        })
-    }
-
-    dataURItoBlob(dataURI) {
-        // convert base64/URLEncoded data component to raw binary data held in a string
-        var byteString;
-        if (dataURI.split(',')[0].indexOf('base64') >= 0)
-            byteString = atob(dataURI.split(',')[1]);
-        else
-            byteString = unescape(dataURI.split(',')[1]);
-
-        // separate out the mime component
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-        // write the bytes of the string to a typed array
-        var ia = new Uint8Array(byteString.length);
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-
-        return new Blob([ia], { type: mimeString });
-    }
-
-    rotateCW() {
-        this.cropper.rotate(90);
-    }
-
-    rotateCounterCW() {
-        this.cropper.rotate(-90);
-    }
-
-    renderRedirect = () => {
-        let redirectLink = 'palette';
-        if (this.state.redirect) {
-            return <Redirect to={redirectLink} />
-        }
+    this.state = {
+      src: src,
+      cropResult: null,
+      redirect: false,
     };
 
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
+    this.submitImage = this.submitImage.bind(this);
+  }
+
+  //
+  // TO REMOVE
+  //
+  /* onChange(e) 
+  {
+    e.preventDefault();
+
+    let files;
+
+    if (e.dataTransfer)
+    {
+      files = e.dataTransfer.files;
+    }
+    else if (e.target)
+    {
+      files = e.target.files;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.setState({ src: reader.result });
     };
+    reader.readAsDataURL(files[0]);
+  } */
+
+  dataURItoBlob(dataURI)
+  {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else
+      byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++)
+    {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], { type: mimeString });
+  }
+
+  //
+  // axios request
+  //
+  submitImage = () =>
+  {    
+    var fd = new FormData();
+
+    fd.append('image', this.dataURItoBlob(this.cropper.getCroppedCanvas().toDataURL()), "edited image");
+    axios({
+      url: '/api/image/',
+      method: 'POST',
+      data: fd,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': "bearer " + getToken()
+      }
+    })
+    .then(res => {
+      this.setState({ redirect: true });
+    })
+  }
+
+  //
+  // edit image functions
+  //
+  resetImage = () =>
+  {
+    this.cropper.reset();
+  }
+
+  cropImage = () =>
+  {
+    if (typeof this.cropper.getCroppedCanvas() === 'undefined')
+    {
+      return;
+    }
+
+    this.setState({ cropResult: this.cropper.getCroppedCanvas().toDataURL() });
+  }
+
+  rotateCW = () =>
+  {
+    this.cropper.rotate(90);
+  }
+
+  rotateCounterCW = () =>
+  {
+    this.cropper.rotate(-90);
+  }
+
+  scaleXY = () =>
+  {
+    let sizeX = Number(this.state.scaleX);
+    let sizeY = Number(this.state.scaleY);
+
+    if (isNaN(sizeX) || isNaN(sizeY))
+    {
+      alert('Input must be a number.');
+      return;
+    }
+
+    this.cropper.scale(this.state.scaleX, this.state.scaleY);
+  }
+
+  setDocReadySize = () =>
+  {
+    //
+  }
+
+  handleScaleChange = (e) =>
+  {
+    let value = e.target.value;
     
-    render() {
-        return (
-            <div>
-                {this.renderRedirect()}
-                <br />
-                <div style={{ width: '100%' }}>
-                    <Cropper
-
-                        style={{ height: 650, width: '100%' }}
-                        guides={false}
-                        src={this.state.src}
-                        ref={cropper => { this.cropper = cropper; }}
-                    />
-                </div>
-                <br />
-                <button onClick={this.setRedirect} style={{ float: 'right' }}>Cancel</button>
-                <button onClick={this.submitImage} style={{ float: 'right' }}>Submit</button>
-                <button onClick={this.rotateCW} style={{ float: 'right' }}>Rotate clockwise</button>
-                <button onClick={this.rotateCounterCW} style={{ float: 'right' }}>Rotate counter clockwise</button>
-
-                <br />
-                <br />
-                <br />
-                <br />
-
-            </div>
-        );
+    if (e.target.id === 'edit_scaleX')
+    {
+      this.setState({ scaleX: value });
     }
+    else if (e.target.id === 'edit_scaleY')
+    {
+      this.setState({ scaleY: value });
+    }
+  }
+
+  //
+  // direct
+  //
+  setRedirect = () =>
+  {
+    this.setState({ redirect: true });
+  };
+
+  renderRedirect = () => 
+  {
+    let redirectLink = 'palette';
+    if (this.state.redirect)
+    {
+      return <Redirect to={redirectLink} />;
+    }
+  };
+  
+  //
+  // redirect
+  //
+  render() 
+  {
+    return (
+      <div>
+        <Title title='EDIT IMAGE' />
+        {this.renderRedirect()}
+        {this.renderFunction()}
+        {this.renderEditImage()}
+      </div>
+    );
+  }
+
+  renderFunction()
+  {
+    return (
+      <div>
+        <div className="fnbar">
+          <button onClick={this.setRedirect}>Cancel</button>
+          <button onClick={this.submitImage}>Submit</button>
+          <button onClick={this.resetImage}>Reset</button>
+        </div>
+        
+        <div className="fnbar">
+          <button onClick={this.scaleXY}>Scale</button>
+          <div style={{ float: 'right', margin: '0px', padding: '0px 0px' }}>
+            <TextInput
+              disabled={false}
+              id="edit_scaleX"
+              placeholder="X"
+              onChange={this.handleScaleChange}/>
+            <TextInput
+              disabled={false}
+              id="edit_scaleY"
+              placeholder="Y"
+              onChange={this.handleScaleChange}/>
+          </div>
+          <button onClick={this.rotateCW}>Rotate Clockwise</button>
+          <button onClick={this.rotateCounterCW}>Rotate Counter-clockwise</button>
+        </div>
+      </div>
+    );
+  }
+
+  renderEditImage()
+  {
+    return (
+      <div id="edit_cropper">
+        <Cropper
+          style={{ height: 550, width: '95%', margin: 'auto' }}
+          guides={false}
+          src={this.state.src}
+          ref={cropper => { this.cropper = cropper; }} />
+      </div>
+    );
+  }
+
 }
 
