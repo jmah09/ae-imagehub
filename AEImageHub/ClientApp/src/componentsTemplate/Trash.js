f1181f6e9371085649f7ae4b3a261e42c23f807e
sadeq <sadeqhsb@tuta.io>
2019-04-02 16:14:01 -0700

    IE11 revert trash reload

--- AEImageHub/ClientApp/src/componentsTemplate/Trash.js
+++ AEImageHub/ClientApp/src/componentsTemplate/Trash.js
@@ -145,9 +145,9 @@ export class Trash extends Component {
     axios.all(promises)
       .then(function () {
         that.setState({ photos: notSelected });
-        if (isIE() && selected.length > 0) {
+        /*if (isIE() && selected.length > 0) {
           window.location.reload();
-        }
+        }*/
       })
       .catch(function (err) { console.log(err.message); });
   }
@@ -185,9 +185,9 @@ export class Trash extends Component {
     axios.all(promises)
       .then(function () {
         that.setState({ photos: notSelected });
-        if (isIE() && selected.length > 0) {
+        /*if (isIE() && selected.length > 0) {
           window.location.reload();
-        }
+        }*/
       })
       .catch(function (err) { console.log("Recover failed: " + err.response); });
 
