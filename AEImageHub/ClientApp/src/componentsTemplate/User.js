bbbf3c4ef95e592c7eaac3432105ab02ab566fac
seraphinush <seraphinus.hong@gmail.com>
2019-04-01 18:25:58 -0700

    update ui components

--- AEImageHub/ClientApp/src/componentsTemplate/User.js
+++ AEImageHub/ClientApp/src/componentsTemplate/User.js
@@ -1,163 +1,163 @@
 import React, { Component } from 'react';
 import { Title } from './Title';
 import axios from 'axios';
-import {adalConfig, authContext, getToken} from '../adalConfig';
+import { adalConfig, authContext, getToken } from '../adalConfig';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import {adalGetToken} from "react-adal";
+import { adalGetToken } from "react-adal";
 
 export class User extends Component {
 
-    constructor(props) {
-        super(props);
-        this.getUsers = this.getUsers.bind(this);
-        this.getUsers();
+  constructor(props) {
+    super(props);
+    this.getUsers = this.getUsers.bind(this);
+    this.getUsers();
 
-        this.state = {
-            users: [],
+    this.state = {
+      users: [],
 
-        };
-    }
+    };
+  }
 
-    viewPalette(userid) {
-        this.props.history.push("/palette?" + userid);
-    }
+  viewPalette(userid) {
+    this.props.history.push("/palette?" + userid);
+  }
 
-    viewTrash(userid) {
-        this.props.history.push("/trash?" + userid);
-    }
+  viewTrash(userid) {
+    this.props.history.push("/trash?" + userid);
+  }
 
-    makeAdmin(userId) {
-        console.log(userId);
-        adalGetToken(authContext, adalConfig.endpoints.api)
-            .then(function (token) {
-                axios.post("/api/graph/" + userId, null, {headers: {'Authorization': "bearer " + token}})
-                    .then(res => {
-                        console.log(res);
-                        alert("The selected User has been made an admin.");
-                    })
-            }).catch(function (err) {
-            console.log("Error: Couldn't get token")
-        });
-        
-    }
+  makeAdmin(userId) {
+    console.log(userId);
+    adalGetToken(authContext, adalConfig.endpoints.api)
+      .then(function (token) {
+        axios.post("/api/graph/" + userId, null, { headers: { 'Authorization': "bearer " + token } })
+          .then(res => {
+            console.log(res);
+            alert("The selected User has been made an admin.");
+          })
+      }).catch(function (err) {
+        console.log("Error: Couldn't get token")
+      });
 
-    getUsers() {
-        const that = this;
-        adalGetToken(authContext, adalConfig.endpoints.api)
-            .then(function (token) {
-                axios.get("/api/graph", {headers: {'Authorization': "bearer " + token}})
-                    .then(res => {
-                        var users = [];
-                        console.log(res);
-                        res.data.map((user, index) => {
-                            users.push({
-                                name: user.displayName,
-                                email: user.mail,
-                                uid: user.id,
-                                userPrincipalName: user.userPrincipalName
-                            })
-                        });
-                        that.setState({users: users});
-                    })
-            }).catch(function (err) {
-            console.log("Error: Couldn't get token")
-        });
-    }
+  }
 
-    render() {
-        return (
-            <div>
-            <div>
-            <div>
+  getUsers() {
+    const that = this;
+    adalGetToken(authContext, adalConfig.endpoints.api)
+      .then(function (token) {
+        axios.get("/api/graph", { headers: { 'Authorization': "bearer " + token } })
+          .then(res => {
+            var users = [];
+            console.log(res);
+            res.data.map((user, index) => {
+              users.push({
+                name: user.displayName,
+                email: user.mail,
+                uid: user.id,
+                userPrincipalName: user.userPrincipalName
+              })
+            });
+            that.setState({ users: users });
+          })
+      }).catch(function (err) {
+        console.log("Error: Couldn't get token")
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
             <Title title='MANAGEMENT: USER' />
-            </div>
-            </div>
-            <div id="palcontent">
-            {this.renderContent()}
-            </div>
-            </div>
+          </div>
+        </div>
+        <div id="palcontent">
+          {this.renderContent()}
+        </div>
+      </div>
     );
-    }
+  }
 
 
-    // TODO
-    renderContent() {
-        const statusStyle = {
-            margin: '0px 0px 0px 70px'
-        };
+  // TODO
+  renderContent() {
+    const statusStyle = {
+      margin: '0px 0px 0px 70px'
+    };
 
-        const columns = [
-            {
-                Header: 'Name',
-                accessor:'name',
-            },
-            {
-                Header: 'Email',
-                accessor: 'email',
-                show: false
-            },
-            {
-                Header: 'UId',
-                accessor: 'uid',
-                show: true
-            },
-            {
-                Header: 'userPrincipalName',
-                accessor: 'userPrincipalName',
-                show: false
-            }
-        ];
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+        show: false
+      },
+      {
+        Header: 'UId',
+        accessor: 'uid',
+        show: true
+      },
+      {
+        Header: 'userPrincipalName',
+        accessor: 'userPrincipalName',
+        show: false
+      }
+    ];
 
-        const sub_columns = columns.slice(0);
-        sub_columns.push({
-            id: 'button',
-            accessor: 'uid',
-            Cell: ({value}) => (<div class="fnbar">
-            <button onClick={()=>{
-            this.makeAdmin(value);
+    const sub_columns = columns.slice(0);
+    sub_columns.push({
+      id: 'button',
+      accessor: 'uid',
+      Cell: ({ value }) => (<div class="fnbar">
+        <button onClick={() => {
+          this.makeAdmin(value);
         }}>Make Admin
         </button>
-        </div>)
+      </div>)
     });
-        
-        sub_columns.push({
-            id: 'button2',
-            accessor: 'uid',
-            Cell: ({value}) => (<div className="fnbar">
-            <button onClick={() => {
-            this.viewPalette(value);
-            // TODO
+
+    sub_columns.push({
+      id: 'button2',
+      accessor: 'uid',
+      Cell: ({ value }) => (<div className="fnbar">
+        <button onClick={() => {
+          this.viewPalette(value);
+          // TODO
         }}>View Palette
         </button>
-        </div>)
+      </div>)
     });
 
-        sub_columns.push({
-            id: 'button3',
-            accessor: 'uid',
-            Cell: ({value}) => (<div className="fnbar">
-                <button onClick={() => {
-                    this.viewTrash(value);
-                    // TODO
-                }}>View Trash
+    sub_columns.push({
+      id: 'button3',
+      accessor: 'uid',
+      Cell: ({ value }) => (<div className="fnbar">
+        <button onClick={() => {
+          this.viewTrash(value);
+          // TODO
+        }}>View Trash
                 </button>
-            </div>)
-        });
+      </div>)
+    });
 
-
-        return (
-            <div>
-            <br />
-            <br />
-            <ReactTable
-              data={this.state.users}
-              columns={sub_columns}
-              defaultPageSize={10}
-              minRows={10}
-            />
+    return (
+      <div>
+        <div className='fnbar'></div>
+        <div>
+          <ReactTable
+            data={this.state.users}
+            columns={sub_columns}
+            defaultPageSize={10}
+            minRows={10}
+          />
         </div>
+      </div>
     )
-    }
+  }
 
 }
