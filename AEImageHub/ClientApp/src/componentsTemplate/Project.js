b6236a32ecd2fc7ee7f167c29b0ed73d5b56f200
sadeq <sadeqhsb@tuta.io>
2019-04-03 23:55:56 -0700

    make project open in a new tab

--- AEImageHub/ClientApp/src/componentsTemplate/Project.js
+++ AEImageHub/ClientApp/src/componentsTemplate/Project.js
@@ -201,9 +201,9 @@ export class Project extends Component {
 
     for (let i = 0; i < projects.length; i++) {
       let project = {};
-      project.name = <Link to={{ pathname: "/search?project=" + '"' + projects[i].ProjectName + '"' }}
-                           >
-        {projects[i].ProjectName}</Link>;
+      project.name = <Link to= {{ pathname: "/search?project=" + '"' + projects[i].ProjectName + '"' } }
+                           target="_blank"  >
+        {projects[i].ProjectName} </Link>;
 
 
       project.uniqueName = projects[i].ProjectName;
